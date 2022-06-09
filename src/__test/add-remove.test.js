93aa610e23d2b873740b7e5bb63584248db8fe57
aliatayee <aliaqaatayee@gmail.com>
2022-06-10 00:42:10 +0430

    Fix linter errors

--- src/__test/add-remove.test.js
+++ src/__test/add-remove.test.js
@@ -1,33 +1,31 @@
 /**
  * @jest-environment jsdom
  */
- import {
-    displayTasks,
-  } from '../modules/functionalities.js';
-  import Task from '../modules/toDoTask.js';
-  
-  const Tasks = new Task();
-  
-  document.body.innerHTML = '<ul class="nav__items"></ul>';
-  const listContainer = document.querySelector('.nav__items');
-  
-  describe('Test Add Task and local storage', () => {
-    window.localStorage = Storage.prototype;
-    test('Add new Task', () => {
-      const task = {
-        index: 0,
-        description: 'This is sample test',
-        completed: false,
-      };
-      Tasks.addTask(task);
-      displayTasks(Tasks.tasksArray, listContainer);
-      expect(Tasks.tasksArray).toHaveLength(1);
-    });
-  
-    test('Test local storage', () => {
-      JSON.parse(localStorage.getItem('TASKS'));
-      expect(localStorage).toHaveLength(1);
-    });
-  });
-  
+import {
+  displayTasks,
+} from '../modules/functionalities.js';
+import Task from '../modules/toDoTask.js';
 
+const Tasks = new Task();
+
+document.body.innerHTML = '<ul class="nav__items"></ul>';
+const listContainer = document.querySelector('.nav__items');
+
+describe('Test Add Task and local storage', () => {
+  window.localStorage = Storage.prototype;
+  test('Add new Task', () => {
+    const task = {
+      index: 0,
+      description: 'This is sample test',
+      completed: false,
+    };
+    Tasks.addTask(task);
+    displayTasks(Tasks.tasksArray, listContainer);
+    expect(Tasks.tasksArray).toHaveLength(1);
+  });
+
+  test('Test local storage', () => {
+    JSON.parse(localStorage.getItem('TASKS'));
+    expect(localStorage).toHaveLength(1);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import Task from './modules/toDoTask.js';
+
 const todoItemsList = document.querySelector('#list_navv');
 const clearAllBtn = document.querySelector('#clearAllBTN');
 const task = new Task();
@@ -13,14 +14,14 @@ const toggle = (index) => {
       item.completed = !item.completed;
     }
   });
-task.addTask();
-window.location.reload();
+  task.addTask();
+  window.location.reload();
 };
 
 const clearAll = () => {
   task.tasksArray.forEach((data) => {
     if (data.completed === true) {
-      task.tasksArray =  task.tasksArray.filter((item) => item.index != data.index);
+      task.tasksArray = task.tasksArray.filter((item) => item.index != data.index);
     }
   });
   task.tasksArray.forEach((task, i) => {
