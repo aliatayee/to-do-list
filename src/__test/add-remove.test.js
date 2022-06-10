f2eb655acc84276e842d05d1771e3ad2230f7520
aliatayee <aliaqaatayee@gmail.com>
2022-06-10 10:17:54 +0430

    Add remove unit test functionality

--- src/__test/add-remove.test.js
+++ src/__test/add-remove.test.js
@@ -1,31 +1,40 @@
 /**
  * @jest-environment jsdom
  */
-import {
-  displayTasks,
-} from '../modules/functionalities.js';
-import Task from '../modules/toDoTask.js';
-
-const Tasks = new Task();
-
-document.body.innerHTML = '<ul class="nav__items"></ul>';
-const listContainer = document.querySelector('.nav__items');
-
-describe('Test Add Task and local storage', () => {
-  window.localStorage = Storage.prototype;
-  test('Add new Task', () => {
-    const task = {
-      index: 0,
-      description: 'This is sample test',
-      completed: false,
-    };
-    Tasks.addTask(task);
-    displayTasks(Tasks.tasksArray, listContainer);
-    expect(Tasks.tasksArray).toHaveLength(1);
+ import {
+    displayTasks,
+    removeTask,
+  } from '../modules/functionalities.js';
+  import Task from '../modules/toDoTask.js';
+  
+  const Tasks = new Task();
+  
+  document.body.innerHTML = '<ul class="nav__items"></ul>';
+  const listContainer = document.querySelector('.nav__items');
+  
+  describe('Test Add Task and local storage', () => {
+    window.localStorage = Storage.prototype;
+    test('Add new Task', () => {
+      const task = {
+        index: 0,
+        description: 'This is sample test',
+        completed: false,
+      };
+      Tasks.addTask(task);
+      displayTasks(Tasks.tasksArray, listContainer);
+      expect(Tasks.tasksArray).toHaveLength(1);
+    });
+  
+    test('Test local storage', () => {
+      JSON.parse(localStorage.getItem('TASKS'));
+      expect(localStorage).toHaveLength(1);
+    });
   });
-
-  test('Test local storage', () => {
-    JSON.parse(localStorage.getItem('TASKS'));
-    expect(localStorage).toHaveLength(1);
+  
+  describe('Test Remove task from list', () => {
+    window.localStorage = Storage.prototype;
+    test('Remove task', () => {
+      removeTask(Tasks.tasksArray, Tasks.addTask, '0');
+      expect(Tasks.tasksArray).toHaveLength(0);
+    });
   });
-});
