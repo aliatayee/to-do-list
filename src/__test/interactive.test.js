b35aee88c93cc44fede754f92bbc115158d4f0df
Profsain <profsaincrown@gmail.com>
2022-06-10 09:03:40 +0100

    Add clear all completed tasks test

--- src/__test/interactive.test.js
+++ src/__test/interactive.test.js
@@ -26,3 +26,41 @@ describe('Test Edit task ', () => {
   });
 });
 
+describe('Test Mark task as completed ', () => {
+  window.localStorage = Storage.prototype;
+  test('Edit Task Mark as completed', () => {
+    const task = {
+      index: 1,
+      description: 'This is Task 1',
+      completed: false,
+    };
+    Tasks.addTask(task);
+    toggle(task.index, Tasks);
+    const newTask = Tasks.tasksArray.find((task) => task.index === 1);
+    expect(newTask.completed).toBeTruthy();
+  });
+
+  test('Test Clear All Completed Tasks', () => {
+    const task2 = {
+      index: 2,
+      description: 'This is Task 2',
+      completed: true,
+    };
+    const task3 = {
+      index: 3,
+      description: 'This is Task 3',
+      completed: true,
+    };
+    const task4 = {
+      index: 4,
+      description: 'This is Task 4',
+      completed: true,
+    };
+    Tasks.addTask(task2);
+    Tasks.addTask(task3);
+    Tasks.addTask(task4);
+    clearAll(Tasks);
+    const tasks = JSON.parse(localStorage.getItem('todos'));
+    expect(tasks.length).toEqual(1);
+  });
+});
